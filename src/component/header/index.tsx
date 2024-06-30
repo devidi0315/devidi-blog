20896f2f9192c5736b3076d5f733a6a3517873bd
vidi <devidi0315@gmail.com>
2024-07-01 08:38:47 +0900

    commit

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <header className=""></header>
+    <header className="flex flex-row justify-between">
+      <div>Logo</div>
+      <nav>
+        <ul className="flex flex-row gap-4">
+          <Link href="/">Home</Link>
+          <Link href="/post">Post</Link>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
