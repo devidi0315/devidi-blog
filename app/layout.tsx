a1cfc07b00151885d9b1b42e393aaea410eac015
vidi <devidi0315@gmail.com>
2024-07-01 23:54:46 +0900

    fixed: ignore core

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import Header from "@/src/component/Header";
 
 const inter = Inter({ subsets: ["latin"] });
---
+++ src/component/Header/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex flex-row justify-between p-4 rounded-lg bg-cardBg">
+      <div>Logo</div>
+      <nav>
+        <ul className="flex flex-row gap-4">
+          <Link href="/">Home</Link>
+          <Link href="/post">Post</Link>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
